bddedfc690aba587700e1767274e4c5c6fbe209e
Valeria Berrocal <valeriaberrocal@MacBook-Pro-de-Valeria-2.local>
2019-04-15 12:21:18 -0500

    haciendo pruebas con omdb

---
+++ main.js
@@ -0,0 +1,24 @@
+let randomMovieArray = ['Deadpool', 'The Hangover', '21 Jump Street', 'The perfect Date'];
+
+let imagen = document.getElementById('imagen');
+
+const apicall = () => {
+    let randomNumber = Math.floor((Math.random() * randomMovieArray.length - 1) + 1);
+    let randomMovie = randomMovieArray[randomNumber];
+    fetch('http://www.omdbapi.com/?t=' + encodeURI(randomMovie) + '&apikey=691b66e1')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            let image = data.Poster;
+            if (image !== 'N/A') {
+                imagen.src = image;
+            }
+            console.log(data);
+        })
+};
+
+const btn = document.getElementById('btn');
+btn.addEventListener('click', () => {
+    apicall();
+});
