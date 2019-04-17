2b7380c2e9638f75da5b4bef7b00b8ca0f1e6c52
magalyelguera <elguera.magaly@gmail.com>
2019-04-17 08:53:02 -0500

    j

--- src/main.js
+++ src/main.js
@@ -5,22 +5,7 @@ let premiers = document.getElementById('premiers');
 const container = document.getElementById('container');
 const demo = document.getElementById('demo');
 container.style.display='none';
-const apicall = () => {
-    let randomNumber = Math.floor((Math.random() * randomMovieArray.length - 1) + 1);
-    let randomMovie = randomMovieArray[randomNumber];
-    fetch('http://www.omdbapi.com/?t=' + encodeURI(randomMovie) + '&apikey=691b66e1')
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            let image = data.Poster;
-            if (image !== 'N/A') {
-                imagen.src = image;
-            }
-            console.log(data.Year);
-            hi.innerHTML=data.Plot;
-            name.innerHTML=data.Title;
-        })
+
 let demo = document.getElementById('demo');
 const newMovies = document.getElementById('newMovies');
 const newBtn = document.getElementById('new');
